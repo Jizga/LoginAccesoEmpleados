7871bee9ab1079e75062257919c60befa02d9228
Jizga <jizga93@gmail.com>
2021-03-12 10:18:57 +0100

    Movimientos entre JSP bien hecho

--- src/main/java/curso/generation/demo/DemoController.java
+++ src/main/java/curso/generation/demo/DemoController.java
@@ -1,20 +1,45 @@
 package curso.generation.demo;
 
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
+// -------------------- Movimiento entre páginas JSP ---------------------------
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index() {
-		
+
 		return "index";
-		
 	}
 
-	
+		
+	@PostMapping("entrada")
+	public String getUsers(@RequestParam String user, @RequestParam String password, Model model, DatosLogin usuario) {
+		
+		usuario = new DatosLogin();
+		String jspDestino = null;
+
+		usuario.setUser(user);
+		usuario.setPassword(password);
+
+		if (usuario.getUser().equalsIgnoreCase("admin") && usuario.getPassword().equals("1234")) {
+			jspDestino = "admi";
+
+		} else if (usuario.getUser().equalsIgnoreCase("pepe") && usuario.getPassword().equals("1234")) {
+			jspDestino = "entrada";
+
+		} else {
+			jspDestino = "error";
+
+		}
+
+		model.addAttribute("usuario", usuario);
+
+
+		return jspDestino;
+	}
 }
